8bdd31ff655b2a43806b46736bdbb0a597f033d8
jmacker101 <jm470t@gmail.com>
2024-05-11 11:15:18 -0400

    making comments to help myself out later

---
+++ config/connections.js
@@ -0,0 +1,18 @@
+// this code is a  for Development
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres'
+  }
+);
+
+module.exports = sequelize;
+
+
+// we have to get the development to deployment
